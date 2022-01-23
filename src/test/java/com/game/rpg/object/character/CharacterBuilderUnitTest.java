ffaa72a57db3ef4a091f1a8f4028d3bbe62d7c67
adrianmarino <adrianmarino@gmail.com>
2022-01-23 11:22:53 -0300

    refactor: remove unrequired config

--- src/test/java/com/game/rpg/object/character/CharacterBuilderUnitTest.java
+++ src/test/java/com/game/rpg/object/character/CharacterBuilderUnitTest.java
@@ -3,6 +3,7 @@ package com.game.rpg.object.character;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static com.game.rpg.object.character.CharacterBuilder.aCharacter;
 import static com.game.rpg.util.Position.zero;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
@@ -14,7 +15,7 @@ class CharacterBuilderUnitTest {
     @DisplayName("When create a default character it has 100% health, is alive and it's on first level")
     void test1() {
         // Perform
-        var character = CharacterBuilder.aCharacter().build();
+        var character = aCharacter().build();
 
         // Assets
         assertThat(character.getLevel(), is(1));
@@ -29,18 +30,18 @@ class CharacterBuilderUnitTest {
     @Test
     @DisplayName("When create a character with a negative level it throws an exception")
     void test2() {
-        assertThrows(IllegalArgumentException.class, () -> CharacterBuilder.aCharacter().level(-1).build());
+        assertThrows(IllegalArgumentException.class, () -> aCharacter().level(-1).build());
     }
 
     @Test
     @DisplayName("When create a character with a negative health it throws an exception")
     void test3() {
-        assertThrows(IllegalArgumentException.class, () -> CharacterBuilder.aCharacter().health(-10).build());
+        assertThrows(IllegalArgumentException.class, () -> aCharacter().health(-10).build());
     }
 
     @Test
     @DisplayName("When create a character with an invalid position it throws an exception")
     void test4() {
-        assertThrows(IllegalArgumentException.class, () -> CharacterBuilder.aCharacter().position(-1, 3).build());
+        assertThrows(IllegalArgumentException.class, () -> aCharacter().position(-1, 3).build());
     }
 }
